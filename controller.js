db286cb796a187f6a5f56219b774009df07df0f2
Ricky <rickyyohanes25@gmail.com>
2020-09-09 14:06:02 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -1,8 +1,19 @@
 "use strict";
 
-var response = require('./res');
+var response = require("./res");
 var conn = require("./koneksi");
 
 exports.index = function (req, res) {
   response.ok("rest", res);
+};
+
+// menampil kan semua data mahasiswa
+exports.tampilsemuasiswa = function (req, res) {
+  conn.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+    if (error) {
+      conn.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
 };
